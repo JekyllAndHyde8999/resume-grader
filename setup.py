877679fc6998d7a50db5a6f7e1f8e14dc56f6413
JekyllAndHyde8999 <sravanvinakota@gmail.com>
2024-02-13 11:50:54 -0500

    Updated dependencies

--- setup.py
+++ setup.py
@@ -3,14 +3,15 @@ from setuptools import find_packages, setup
 setup(
     name="resumegrader",
     packages=find_packages(),
-    version="0.1.5",
+    version="0.1.6",
     description="A library that uses LLMs to rank resumes for a job description",
-    author="Me",
+    author="Sravan Vinakota",
     install_requires=[
         "scikit-learn",
         "langchain",
         "langchain-community",
         "nltk",
         "spacy",
-    ],
+        "pypdf"
+    ]
 )
